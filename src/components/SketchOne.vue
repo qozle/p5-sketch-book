48d42e7640cd1646b7b31a1dd6bb2752297df4d2
qozle <mf.qozle@gmail.com>
2020-09-26 09:32:13 -0400

    init commit

---
+++ src/components/SketchOne.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="sketchOne">
+    <div id="p5canvas"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SketchOne",
+  mounted() {
+    const script = function(p5) {
+      var speed = 2;
+      var posX = 0;
+
+      p5.setup = () => {
+        //          p5.createCanvas(500, 500);
+        var canvas = p5.createCanvas(700, 700);
+        canvas.parent("p5canvas");
+        p5.ellipse(p5.width / 2, p5.height / 2, 500, 500);
+      };
+
+      p5.draw = () => {
+        p5.background(0);
+        const degree = p5.frameCount * 3;
+        const y = p5.sin(p5.radians(degree)) * 50;
+        p5.push();
+        p5.translate(0, p5.height / 2);
+        p5.ellipse(posX, y, 50, 50);
+        p5.pop();
+        posX += speed;
+        if (posX > p5.width || posX < 0) {
+          speed *= -1;
+        }
+      };
+    };
+
+    const P5 = require("p5");
+    new P5(script);
+  }
+};
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SketchOne />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import SketchOne from "@/components/SketchOne.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    SketchOne
   }
 };
 </script>
